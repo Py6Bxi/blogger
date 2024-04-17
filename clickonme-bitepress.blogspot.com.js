dd40f88ef665f81d9db66d87acc6793e15842a86
bitepress <166421169+bitepress@users.noreply.github.com>
2024-04-17 21:18:29 +0700

    clickonme-bitepress.blogspot.com.js

--- clickonme-bitepress.blogspot.com.js
+++ clickonme-bitepress.blogspot.com.js
@@ -1,34 +1,57 @@
 var Arlina = {
-  init : function(n) {
-    this.run(n);
+  /**
+   * @param {Object} action
+   * @return {undefined}
+   */
+  init : function(action) {
+    this.run(action);
+    /** @type {string} */
     document.cookie = "nct=0;";
   },
-  readData : function(n) {
-    var e = n + "=";
-    var t = decodeURIComponent(document.cookie).split(";");
-    var o = 0;
-    for (;o < t.length;o++) {
-      var c = t[o];
+  /**
+   * @param {string} name
+   * @return {?}
+   */
+  readData : function(name) {
+    /** @type {string} */
+    var nameEQ = name + "=";
+    /** @type {Array.<string>} */
+    var codeSegments = decodeURIComponent(document.cookie).split(";");
+    /** @type {number} */
+    var i = 0;
+    for (;i < codeSegments.length;i++) {
+      /** @type {string} */
+      var c = codeSegments[i];
       for (;" " == c.charAt(0);) {
+        /** @type {string} */
         c = c.substring(1);
       }
-      if (0 == c.indexOf(e)) {
-        return c.substring(e.length, c.length);
+      if (0 == c.indexOf(nameEQ)) {
+        return c.substring(nameEQ.length, c.length);
       }
     }
     return "";
   },
-  run : function(n) {
-    var e = n.click;
-    var t = n.interval;
-    var o = document.getElementsByClassName("Arlina");
-    var c = 0;
-    var r = o.length;
-    for (;c < r;c++) {
-      o[c].addEventListener("click", function() {
+  /**
+   * @param {Object} options
+   * @return {undefined}
+   */
+  run : function(options) {
+    var which = options.click;
+    var interval = options.interval;
+    /** @type {NodeList} */
+    var codeSegments = document.getElementsByClassName("Arlina");
+    /** @type {number} */
+    var i = 0;
+    /** @type {number} */
+    var valuesLen = codeSegments.length;
+    for (;i < valuesLen;i++) {
+      codeSegments[i].addEventListener("click", function() {
         tn = Arlina.readData("nct");
+        /** @type {number} */
         po = tn - 1 + 2;
-        if (Arlina.changer(e, t)) {
+        if (Arlina.changer(which, interval)) {
+          /** @type {string} */
           document.cookie = "nct=" + po + ";";
         } else {
           Arlina.n("pointer-events:none;");
@@ -36,18 +59,32 @@ var Arlina = {
       });
     }
   },
-  changer : function(n, e) {
-    return tc = this.readData("nct") - 1 + 2, tc != n || (setTimeout(function() {
+  /**
+   * @param {number} object
+   * @param {?} millis
+   * @return {?}
+   */
+  changer : function(object, millis) {
+    return tc = this.readData("nct") - 1 + 2, tc != object || (setTimeout(function() {
+      /** @type {string} */
       document.cookie = "nct=0;";
       Arlina.n("pointer-events:cursor;");
-    }, e), false);
+    }, millis), false);
   },
-  n : function(n) {
-    var e = document.getElementsByClassName("Arlina");
-    var t = 0;
-    var o = e.length;
-    for (;t < o;t++) {
-      e[t].style = n;
+  /**
+   * @param {string} value
+   * @return {undefined}
+   */
+  n : function(value) {
+    /** @type {NodeList} */
+    var cTaskName1 = document.getElementsByClassName("Arlina");
+    /** @type {number} */
+    var k = 0;
+    /** @type {number} */
+    var numrows = cTaskName1.length;
+    for (;k < numrows;k++) {
+      /** @type {string} */
+      cTaskName1[k].style = value;
     }
   }
 };
